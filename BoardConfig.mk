d92cc870e123b5a2831998354acac79b7d1acbc4
Antonino Scordino <git@antonino.cf>
2022-08-01 21:49:21 +0200

    spacewar: Declare kernel rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,17 @@ TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := false
 TARGET_USES_UEFI := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat loop.max_part iptable_raw.raw_before_defrag= ip6table_raw.raw_before_defrag
+TARGET_KERNEL_SOURCE := kernel/nothing/spacewar
+TARGET_KERNEL_CONFIG := vendor/spacewar_QGKI.config
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := lahaina
