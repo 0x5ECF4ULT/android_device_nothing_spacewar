600a546f10af1b5790d5101cba629a0c3d6da8a4
Cosmin Tanislav <demonsingur@gmail.com>
2022-08-01 22:00:15 +0200

    spacewar: Implement Virtual A/B

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit the proprietary files
 $(call inherit-product, vendor/nothing/spacewar/spacewar-vendor.mk)
 
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
