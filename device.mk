61594f78d802fc778ace745a01846366a9bf6db2
Cosmin Tanislav <demonsingur@gmail.com>
2022-08-01 22:31:23 +0200

    spacewar: Import `bootctrl` HAL and `gpt-utils`

--- device.mk
+++ device.mk
@@ -10,6 +10,15 @@ $(call inherit-product, vendor/nothing/spacewar/spacewar-vendor.mk)
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
