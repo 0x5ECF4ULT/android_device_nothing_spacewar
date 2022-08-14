290d62010e0eaa6126aa83c4b3aa3f7ba583cfeb
0x5ECF4ULT <github@ox5ecf4ult.pw>
2022-08-14 09:50:42 +0000

    spacewar: declare `BOARD_VENDOR`

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ DEVICE_PATH := device/nothing/spacewar
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+BOARD_VENDOR := nothing
+
 # A/B
 AB_OTA_UPDATER := true
 
@@ -59,7 +61,20 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat loop.max_part iptable_raw.raw_before_defrag= ip6table_raw.raw_before_defrag
+BOARD_KERNEL_CMDLINE += \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    cgroup.memory=nokmem,nosocket \
+    iptable_raw.raw_before_defrag=1 \
+    ip6table_raw.raw_before_defrag=1 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    pcie_ports=compat \
+    service_locator.enable=1 \
+    swiotlb=0 \
+    video=vfb:640x400,bpp=32,memsize=3072000
 TARGET_KERNEL_SOURCE := kernel/nothing/spacewar
 TARGET_KERNEL_CONFIG := lineage_spacewar_defconfig
 
